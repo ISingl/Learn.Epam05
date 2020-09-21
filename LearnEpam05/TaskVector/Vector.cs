95df170ad859e6e29d2bc27a1ff12588908397fe
SergeyNogin <ver-s_online@mail.ru>
2020-09-21 13:21:18 +0400

    Add override methods

--- LearnEpam05/TaskVector/Vector.cs
+++ LearnEpam05/TaskVector/Vector.cs
@@ -16,75 +16,83 @@ namespace TaskVector
         /// <summary>
         /// Sum of the vectors
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="a">Vector a</param>
+        /// <param name="b">Vector b</param>
         /// <returns></returns>
         public static Vector operator +(Vector a, Vector b)
         {
             return new Vector(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
         }
         /// <summary>
-        /// разность векторов
+        /// The difference of vectors
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="a">Vector a</param>
+        /// <param name="b">Vector b</param>
         /// <returns></returns>
         public static Vector operator -(Vector a, Vector b)
         {
             return new Vector(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
         }
         /// <summary>
-        /// произведение вектора на число
+        /// Multiplying a vector by a number
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="n"></param>
+        /// <param name="a">Vector a</param>
+        /// <param name="n">Number n</param>
         /// <returns></returns>
         public static Vector operator *(Vector a, double n)
         {
             return new Vector(a.X * n, a.Y * n, a.Z * n);
         }
         /// <summary>
-        /// скалярное произведение векторов
+        /// Scalar multiplication of vectors
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="a">Vector a</param>
+        /// <param name="b">Vector b</param>
         /// <returns></returns>
         public static double operator *(Vector a, Vector b)
         {
             return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
         }
         /// <summary>
-        /// векторное произведение векторов
+        /// Vector multiplication of vectors
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
+        /// <param name="a">Vector a</param>
+        /// <param name="b">Vector b</param>
         /// <returns></returns>
         public static Vector operator ^(Vector a, Vector b)
         {
             return new Vector(a.Y * b.Z - a.Z * b.Y, a.Z * b.X - a.X * b.Z, a.X * b.Y - a.Y * b.X);
         }
         /// <summary>
-        /// Противоположный вектор
+        /// Inverse vector
         /// </summary>
-        /// <param name="a"></param>
+        /// <param name="a">Vector a</param>
         /// <returns></returns>
         public static Vector operator -(Vector a)
         {
             return new Vector(-a.X, -a.Y, -a.Z);
         }
         /// <summary>
-        /// модуль вектора
+        /// The module of the vector
         /// </summary>
-        /// <param name="a"></param>
+        /// <param name="a">Vector a</param>
         /// <returns></returns>
         public static double operator !(Vector a)
         {
             return Math.Sqrt(a.X * a.X + a.Y * a.Y + a.Z * a.Z);
         }
         /// <summary>
-        /// проверка равенства векторов
+        /// Converting a vector to a string
         /// </summary>
-        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"({X},{Y},{Z})";
+        }
+        /// <summary>
+        /// Checking the equality of vectors
+        /// </summary>
+        /// <param name="obj">Vector</param>
         /// <returns></returns>
         public override bool Equals(object obj)
         {
@@ -92,12 +100,19 @@ namespace TaskVector
             return X == n.X && Y == n.Y && Z == n.Z;
         }
         /// <summary>
-        /// преобразование вектора в строку
+        /// Implementation GetHashCode
         /// </summary>
         /// <returns></returns>
-        public override string ToString()
+        public override int GetHashCode()
         {
-            return $"({X}, {Y}, {Z})";
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+                hash = hash * 23 + Z.GetHashCode();
+                return hash;
+            }
         }
     }
 }
